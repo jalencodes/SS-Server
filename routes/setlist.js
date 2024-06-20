3d6f18d81399719f97bf07b75455928bfcd12299
Jalen Vaughn <jalencodes@outlook.com>
2024-06-20 13:53:37 -0400

    Changes made

--- routes/setlist.js
+++ routes/setlist.js
@@ -27,53 +27,6 @@ router.get('/search/artist', async (req, res) => {
 })
 
 
-function formatDate(date) {
-    let [day, month, year] = date.split('-')
-    day = Number(day)
-    month = Number(month) - 1
-    year = Number(year)
-    
-    const concertDate = new Date(year, month, day)
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const prettyDate = concertDate.toLocaleDateString("en-US", options)
-    return prettyDate
-}
-
-
-function formatConcert(concert) {
-    const date = formatDate(concert.eventDate)
-    const venue = concert.venue.name
-    const city = concert.venue.city.name
-    const state = concert.venue.city.stateCode
-    const artistName = concert.artist.name
-
-    const concertHeader = `${artistName} at ${venue} in ${city}, ${state} on ${date}`
-    
-    return concertHeader
-
-}
-
-function getSongs(sets)
-{
-    const allSongs = []
-    for(let set of sets) {
-        const setSongs = set.song;
-        for (let setSong of setSongs) {
-            let songTitle = setSong.name
-            // Checks for medley and makes each song its own song setlist entry
-            if(songTitle.includes(' / ')){
-                const medley = songTitle.split(' / ')
-                for (let medleySong of medley) {
-                    allSongs.push(medleySong)
-                }
-            }
-            else allSongs.push(setSong.name)
-        }
-    }
-    return allSongs;
-}
-
-
 router.get('/search/:mbid/', async (req, res) => {
     try {
         const setlists = await getArtistConcerts(req.params.mbid);
